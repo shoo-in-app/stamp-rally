0ca430d6ac6428288d03b8609fa4ff1802f2b8a9
a-ogilvie <alexander.ogilvie@icloud.com>
2018-06-06 18:05:13 +0900

    Add basic styling

--- components/RallyDetails.js
+++ components/RallyDetails.js
@@ -6,14 +6,29 @@ export default class RallyDetails extends React.Component {
     if (this.props.selectedMarker) {
       return (
         <View>
-          <Text>{this.props.selectedMarker.name}</Text>
-          <Text>{this.props.selectedMarker.description}</Text>
+          <Text style={styles.title}>{this.props.selectedMarker.name}</Text>
+          <Text style={styles.description}>
+            {this.props.selectedMarker.description}
+          </Text>
         </View>
       );
     } else {
-      return <Text>Select a location!</Text>;
+      return <Text style={styles.placeholder}>Select a location!</Text>;
     }
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  placeholder: {
+    fontSize: 20,
+    textAlign: "center"
+  },
+  title: {
+    fontSize: 20,
+    margin: 10
+  },
+  description: {
+    fontSize: 15,
+    margin: 10
+  }
+});
