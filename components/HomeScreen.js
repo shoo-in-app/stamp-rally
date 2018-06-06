bec3f70ad965957dd55d5c07564acb236167ade9
morita657 <morita657@gmail.com>
2018-06-06 11:54:08 +0900

    Pass list component

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import Lists from "./Lists";
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to Stamp Rally!</Text>
+        <Text>Welcome to Stamp Rally!!!!</Text>
         <Button
           title="Open details!"
           onPress={() => this.props.navigation.navigate("Details")}
         />
+        <Lists />
       </View>
     );
   }
