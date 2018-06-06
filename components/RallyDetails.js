65f667021f0c33030c82544ca9ab99fc888b865a
a-ogilvie <alexander.ogilvie@icloud.com>
2018-06-06 10:29:13 +0900

    Add RallyDetails component stub

---
+++ components/RallyDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default class RallyDetails extends React.Component {
+  render() {
+    return <Text>Details here!</Text>;
+  }
+}
+
+const styles = StyleSheet.create({});
