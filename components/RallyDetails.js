dfdcf892cd29f47339eca709b518f865d76a8b22
a-ogilvie <alexander.ogilvie@icloud.com>
2018-06-06 15:13:38 +0900

    Create simple details view

--- components/RallyDetails.js
+++ components/RallyDetails.js
@@ -3,7 +3,16 @@ import { StyleSheet, Text, View, Button } from "react-native";
 
 export default class RallyDetails extends React.Component {
   render() {
-    return <Text>Details here!</Text>;
+    if (this.props.selectedMarker) {
+      return (
+        <View>
+          <Text>{this.props.selectedMarker.name}</Text>
+          <Text>{this.props.selectedMarker.description}</Text>
+        </View>
+      );
+    } else {
+      return <Text>Select a location!</Text>;
+    }
   }
 }
 
