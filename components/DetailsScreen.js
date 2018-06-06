a01ef093401ee89b9c69e2738f072e5981e25776
a-ogilvie <alexander.ogilvie@icloud.com>
2018-06-06 10:20:37 +0900

    Add map to details screen

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,25 +1,54 @@
 import React from "react";
+import { MapView } from "expo";
 import { StyleSheet, Text, View, Button } from "react-native";
 
-export default class DetailsScreen extends React.Component {
+import RallyDetails from "./RallyDetails";
+
+class DetailsScreen extends React.Component {
   render() {
+    const { navigation } = this.props;
+    const locations = navigation.getParam("locations", [
+      {
+        latlng: {
+          latitude: 37.78825,
+          longitude: -122.4324
+        },
+        title: "San Francisco!",
+        description: "A city in America."
+      }
+    ]);
+
     return (
-      <View style={styles.container}>
-        <Text>Details!</Text>
-        <Button
-          title="Go Home!"
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        >
+          {locations.map((location) => (
+            <MapView.Marker
+              key={location.id}
+              coordinate={location.latlng}
+              title={location.title}
+              description={location.description}
+            />
+          ))}
+        </MapView>
+        <RallyDetails style={styles.details} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+  map: { flex: 0.8 },
+  details: {
+    flex: 0.2
   }
 });
+
+export default DetailsScreen;
