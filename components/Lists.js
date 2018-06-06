2e376b483b1d992d6f5f63e53e60ea9ce08b4a3a
morita657 <morita657@gmail.com>
2018-06-06 11:53:55 +0900

    Make simple list

---
+++ components/Lists.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
+import { ListItem } from 'react-native-elements';
+export default class Lists extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [{
+                name: 'Amy Farha',
+                avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                subtitle: 'Vice President'
+            },
+            {
+                name: 'Chris Jackson',
+                avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                subtitle: 'Vice Chairman'
+            }]
+        }
+    }
+    render() {
+        return (
+            <FlatList
+                keyExtractor={(item, index) => {
+                    return index.toString();
+                }}
+                data={this.state.list}
+                renderItem={({ item }) => {
+                    return (<ListItem title={item.name}
+                        leftAvatar={{ source: { uri: item.avatar_url } }}
+                        subtitle={item.subtitle}
+                    />)
+                }}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
