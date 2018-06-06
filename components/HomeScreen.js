e90caa6ab512db536a8b88316e649a3e48a3b381
a-ogilvie <alexander.ogilvie@icloud.com>
2018-06-06 19:07:18 +0900

    Remove mock locations

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -15,42 +15,6 @@ export default class HomeScreen extends React.Component {
       .then((list) => this.setState({ list }));
   }
   render() {
-    const mockLocationData = [
-      {
-        id: 4,
-        name: "Motoazabu Sanchoume",
-        lat: 35.659,
-        lng: 139.722,
-        description: "nearest famima to cc",
-        rally_id: 1,
-        user_id: 2,
-        location_id: 1,
-        visited: false
-      },
-      {
-        id: 5,
-        name: "Motoazabu",
-        lat: 35.6549,
-        lng: 139.726,
-        description: "second nearest famima to cc",
-        rally_id: 1,
-        user_id: 2,
-        location_id: 2,
-        visited: true
-      },
-      {
-        id: 6,
-        name: "Nishiazabu Sanchoume",
-        lat: 35.6594,
-        lng: 139.723,
-        description: "third nearest famima to cc",
-        rally_id: 1,
-        user_id: 2,
-        location_id: 3,
-        visited: false
-      }
-    ];
-
     return (
       <View style={styles.container}>
         <Text>Welcome to Stamp Rally!!!!</Text>
