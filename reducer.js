e75df1f4329103d6cc54a5792c40543f038af91b
a-ogilvie <alexander.ogilvie@icloud.com>
2018-06-07 15:09:57 +0900

    Remove selectRally from reducer (don't need to save it)

--- reducer.js
+++ reducer.js
@@ -2,13 +2,11 @@ import { createStore } from "redux";
 
 const initialState = {
   rallies: [],
-  selectedRally: null,
   userID: ""
 };
 
 const LOAD_RALLIES = "LOAD_RALLIES";
 const SET_USER_ID = "SET_USER_ID";
-const SELECT_RALLY = "SELECT_RALLY";
 
 const loadRallies = (rallies) => ({
   type: LOAD_RALLIES,
@@ -20,24 +18,15 @@ const setUserID = (userID) => ({
   userID
 });
 
-const selectRally = (selectedRally) => ({
-  type: SELECT_RALLY,
-  selectedRally
-});
-
 const reducer = (previousState = initialState, action) => {
   switch (action.type) {
     case LOAD_RALLIES:
       return { ...previousState, rallies: action.rallies };
     case SET_USER_ID:
       return { ...previousState, userID: action.userID };
-    case SELECT_RALLY:
-      return { ...previousState, selectedRally: action.selectedRally };
     default:
       return previousState;
   }
 };
 
-const store = createStore(reducer);
-
-export { store, reducer, loadRallies, setUserID, selectRally };
+export { reducer, loadRallies, setUserID };
